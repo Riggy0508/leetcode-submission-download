8bbd5de542eb94b8f67f1beb7bf3ba215ad3fbda
Rigved Patil <riggy@Visuri-14.lan>
2022-08-23 22:01:52 -0700

    Leetcode

---
+++ index.js
@@ -0,0 +1,7 @@
+var prices=document.getElementsByClassName("h-5 hover:text-blue-s dark:hover:text-dark-blue-s");
+var price_array=[];
+for(i=0;i<prices.length;++i){price_array.push(prices[i].innerHTML);}
+console.log(price_array)
+
+// the above will help you download the list of problem's solved by you on the problem list page.
+
